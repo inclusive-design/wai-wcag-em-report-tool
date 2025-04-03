d6e7ac70b58254e8a6af859cc2375d4eead355aa
Ned Zimmerman <ned@bight.dev>
2025-04-03 10:30:50 -0300

    feat: add better report styles
    Signed-off-by: Ned Zimmerman <ned@bight.dev>

--- src/scripts/files.js
+++ src/scripts/files.js
@@ -10,7 +10,7 @@ export function downloadFileHTML({ contents, name = 'download.txt', type = 'text
   meta.setAttribute("charset", "utf-8");
   htmlDocument.head.appendChild(meta);
 
-  // set lang 
+  // set lang
   htmlDocument.documentElement.setAttribute('lang', lang);
 
   // remove certain elements
@@ -28,27 +28,114 @@ export function downloadFileHTML({ contents, name = 'download.txt', type = 'text
     el.removeAttribute("class");
   });
 
-  // add CSS 
+  // add CSS
   const styleEl = document.createElement("style");
   const styleElContents = document.createTextNode(`
-  table {
-    border-collapse: collapse;
-    width: 100%;
-  }
-  table,
-  td,
-  th {
-    border: 1px solid #3b3b3b;
-  }
-  td:not(:last-child),
-  th {
-    padding: 1em;
-    vertical-align: top;
-    text-align: left;
-  }
-  td:not([class]):last-child {
-    padding: 0 1em;
-  }
+    body {
+        font-family: sans-serif;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    table,
+    td,
+    th {
+        border: 1px solid #3b3b3b;
+    }
+
+    td:not(:last-child),
+    th {
+        padding: 1em;
+        vertical-align: top;
+        text-align: left;
+    }
+
+    th[scope="row"] {
+        width: 20%;
+    }
+
+    td:not(:last-child) {
+        width: 20%;
+    }
+
+    td:not([class]):last-child {
+        padding: 0 1em;
+    }
+
+    main > * + *,
+    div > * + *,
+    td > * + * {
+        margin-top: 1em;
+    }
+
+    h1 {
+        font-size: 2.5rem;
+        margin-bottom: 0;
+    }
+
+    h2 {
+        font-size: 1.75rem;
+        margin-bottom: 0;
+    }
+
+    h3 {
+        font-size: 1.5rem;
+        margin-bottom: 0;
+    }
+
+    h4 {
+        font-size: 1.25rem;
+        margin-bottom: 0;
+    }
+
+    h5 {
+        font-size: 1.125rem;
+        margin-bottom: 0;
+    }
+
+    h6 {
+        font-size: 1rem;
+        margin-bottom: 0;
+        margin-top: 1.75em;
+    }
+
+    dt {
+        font-weight: 600;
+    }
+
+    dt::after {
+        content: ":";
+    }
+
+    dd {
+        margin-inline: 0;
+    }
+
+    dd + dt {
+        margin-top: 1rem;
+    }
+
+    td img {
+        max-width: 5in;
+    }
+
+    td pre {
+        background: #eee;
+        border-radius: 0.5rem;
+        font-size: 1rem;
+        padding: 1rem;
+    }
+
+    td code {
+        background: #eee;
+        border-radius: 0.25rem;
+        display: inline-block;
+        padding: 0.125rem;
+    }
+
   `);
   styleEl.appendChild(styleElContents);
   htmlDocument.head.append(styleEl);
